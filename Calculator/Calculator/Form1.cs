3dfd0d8da65283f8d46f06a72d89dc72e3573129
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-06-20 15:43:27 +0300

    Calculator on C#

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "+":
+                    lblResult.Text = (int.Parse(textBox1.Text) + int.Parse(textBox2.Text)).ToString();
+                    break;
+                case "-":
+                    lblResult.Text = (int.Parse(textBox1.Text) - int.Parse(textBox2.Text)).ToString();
+                    break;
+                case "*":
+                    lblResult.Text = (int.Parse(textBox1.Text) * int.Parse(textBox2.Text)).ToString();
+                    break;
+                case "/":
+                    lblResult.Text = (int.Parse(textBox1.Text) / int.Parse(textBox2.Text)).ToString();
+                    break;
+            }
+        }
+        private void OnOparetors_click(object sender, EventArgs e)
+        {
+            comboBox1.Text = ((Button)sender).Text;
+            // ((Button)sender).Text;
+        }
+       
+    }
+ 
+
+}
