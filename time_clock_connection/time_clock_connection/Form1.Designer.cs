d4e7c3ff817a93a73d147685373c00e73c47e3ca
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-06-20 15:44:43 +0300

    time_clock C#

---
+++ time_clock_connection/time_clock_connection/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace time_clock_connection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.txtId.Location = new System.Drawing.Point(390, 128);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(331, 61);
+            this.txtId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(436, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 54);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "מספר זהות";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(481, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 54);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "סיסמא";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.txtPassword.Location = new System.Drawing.Point(390, 346);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(331, 61);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 689);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtId);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ time_clock_connection/time_clock_connection/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace time_clock_connection
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection Connection;
+        string connectionString = "server=OMRIRAJUAN\\SQLEXPRESS;" +
+            "initial-catalog=time-clock;" +
+            "user id=sa;" +
+            " password = 1234";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect()
+        {
+            try
+            {
+                Connection = new SqlConnection(connectionString);
+                Connection.Open();
+                MessageBox.Show("פתווווווחחחח");
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                
+            }
+        }
+
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != (char)13) return;
+            Connect();
+        }
+    }
+}
